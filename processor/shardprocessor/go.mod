c7fa38aa4aa35642e8eab5f8ac3b386b811924d3
shashankmotruri <motrurishashank000@gmail.com>
2025-07-22 08:38:37 +0000

    Add shard processor

---
+++ processor/shardprocessor/go.mod
@@ -0,0 +1,7 @@
+module github.com/yugabyte/opentelemetry-collector-contrib/processor/shardprocessor
+
+go 1.20
+
+require (
+	go.opentelemetry.io/collector v0.129.0
+)
---
+++ processor/shardprocessor/processor.go
@@ -0,0 +1,75 @@
+package shardprocessor
+
+import (
+	"context"
+	"fmt"
+	"hash/fnv"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/pmetric"
+	"go.opentelemetry.io/collector/processor"
+)
+
+type processorImp struct {
+	config *Config
+	next   consumer.Metrics
+}
+
+func New(cfg *Config, next consumer.Metrics) (processor.Metrics, error) {
+	return &processorImp{
+		config: cfg,
+		next:   next,
+	}, nil
+}
+
+func (sp *processorImp) Start(_ context.Context, _ component.Host) error {
+	return nil
+}
+
+func (sp *processorImp) Shutdown(_ context.Context) error {
+	return nil
+}
+
+func (sp *processorImp) Capabilities() consumer.Capabilities {
+	return consumer.Capabilities{MutatesData: true}
+}
+
+func (sp *processorImp) ConsumeMetrics(ctx context.Context, md pmetric.Metrics) error {
+	rms := md.ResourceMetrics()
+	for i := 0; i < rms.Len(); i++ {
+		rm := rms.At(i)
+		attrs := rm.Resource().Attributes()
+
+		labelKey := sp.config.ShardLabel
+		labelValue, exists := attrs.Get(labelKey)
+
+		var shardID int
+		shouldInjectShardID := false
+
+		if exists && labelValue.Type() == pcommon.ValueTypeStr {
+			strVal := labelValue.Str()
+			if strVal != "" {
+				shardID = sp.calculateShardID(strVal)
+				shouldInjectShardID = true
+			} else {
+				shardID = 0
+			}
+		} else {
+			shardID = 0
+		}
+
+		if shouldInjectShardID {
+			attrs.PutStr("shard_id", fmt.Sprintf("%d", shardID))
+		}
+	}
+
+	return sp.next.ConsumeMetrics(ctx, md)
+}
+
+func (sp *processorImp) calculateShardID(name string) int {
+	hasher := fnv.New32a()
+	_, _ = hasher.Write([]byte(name))
+	return int(hasher.Sum32() % uint32(sp.config.NumShards))
+}
